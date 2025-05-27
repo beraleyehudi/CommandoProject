8f020d560b348f9f626ce4026d02a35679d35ca4
berale <b.y.berale@gmail.com>
2025-05-27 09:46:37 +0300

    Commandi class

--- Commando.cs
+++ Commando.cs
@@ -8,6 +8,28 @@ namespace CommandoProject
 {
     internal class Commando
     {
+        public string Name;
+        public string CodeName;
+        public string[] Tools = { "Hammer", "Chisel", "Knife", "Rope", "Ladder" };
+        public Enums.SoliderStatus Status;
+
+        public Commando(string name, string codeName)
+        {
+            Name = name;
+            CodeName = codeName;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("The solider walking");
+        }
+
+        public void Hide()
+        {
+            Console.WriteLine("The solider hiding");
+        }
+
+
 
     }
 }
