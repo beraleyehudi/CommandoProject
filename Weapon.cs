0fe6374bea1f4397698d4dc231327b8e915c5fe9
berale <b.y.berale@gmail.com>
2025-05-27 09:52:23 +0300

    Weapon Class

---
+++ Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoProject
+{
+    internal class Weapon
+    {
+        public string Name;
+        public string Manufacturer;
+        public int Bullets;
+
+        public Weapon(string name, string manufacturer)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("bummmm!!");
+            this.Bullets--;
+        }
+
+    }
+}
