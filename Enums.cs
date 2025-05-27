9b3d99c9655aad1d0eb5944e0cccc80f949b85d3
berale <b.y.berale@gmail.com>
2025-05-27 09:35:59 +0300

    enum

---
+++ Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoProject
+{
+    public class Enums
+    {
+        public enum SoliderStatus
+        {
+            standing,
+            walk,
+            hide
+
+        }
+
+        //public enum Tools
+        //{
+
+        //}
+
+    }
+}
